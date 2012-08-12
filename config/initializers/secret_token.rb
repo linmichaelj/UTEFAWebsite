abb2bce7bb1b2b2617cc8478e1c97ca2d30cc2da
Michael Lin <michaellin@Michaels-MacBook-Pro.local>
2012-08-12 16:15:19 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UTEFAApp::Application.config.secret_token = '607e60fdb7bd9627373a598dde0f49614e297a613bd4f260f385480a1337c1ce306b5642aae7b9e4b67cd1651305a6625d92df62ead311b8b29ba3e83c65e559'
