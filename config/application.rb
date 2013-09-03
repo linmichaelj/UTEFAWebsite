3ccff681b970d6117886c683834e0628c89d0934
Michael Lin <linmichaelj@gmail.com>
2013-09-02 23:10:19 -0400

    please

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,8 @@ module UTEFAApp
     # parameters by using an attr_accessible or attr_protected declaration.
     #config.active_record.whitelist_attributes = true
 
-    config.assets.initialize_on_precompile=false
+    config.assets.precompile += %w( *.js *.css)
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
